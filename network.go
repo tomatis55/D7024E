8176d687916375471ed6dd718ac367c36f1c0142
tomatis55 <joskarkarlsson@gmail.com>
2022-09-05 10:55:14 +0000

    new branch !

--- network.go
+++ network.go
@@ -1,5 +1,16 @@
 package d7024e
 
+/*
+M1. Network formation. [5p]. Your nodes must be able to form networks as described in the Kadem-
+	lia paper.8 Kademlia is a protocol for facilitating Distributed Hash Tables (DHTs).9 Concretely,
+	the following aspects of the algorithm must be implemented:
+	(a) Pinging. This means that you must implement and use the PING message.
+	(b) Network joining. Given the IP address, and any other data you decide, of any single
+		node, a node must be able to join or form a network with that node.
+	(c) Node lookup. When part of a network, each node must be able to retrieve the contact
+	information of any other node in the same network.
+*/
+
 type Network struct {
 }
 
