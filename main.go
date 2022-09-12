ab49b09efbe7ddb08087b98e2bbd07f31b9c23dd
Cloud327 <roman.ermolaev@hotmail.com>
2022-09-12 10:32:40 +0000

    learning kademlia

--- main.go
+++ main.go
@@ -9,14 +9,18 @@ func main() {
 	// Use fmt.Printf("%s\n", decoded)
 	id := NewKademliaID("5465747261687964726F63616E6E6162696E6F6C")
 	d := id.CalcDistance(id)
-	fmt.Println(d)
 	me := Contact{ID: id, Address: "123", distance: d}
 	table := NewRoutingTable(me)
 	kademlia := Kademlia{routingTable: table}
-	fmt.Println(*kademlia.routingTable.buckets[0])
-	fmt.Println("AAWDAW")
-
 	//fmt.Printf("%s\n", *kademlia.routingTable.me.ID) // Extracts the original id string
+	fmt.Println("routingTable.buckets[0]: ", *kademlia.routingTable.buckets[0])
+	testID := NewRandomKademliaID()
+	fmt.Println("testID: ", testID)
+	testDistance := testID.CalcDistance(id)
+	fmt.Println("Distance between me and testID: ", testDistance)
+	testContact := Contact{ID: testID, Address: "327", distance: testDistance}
+	kademlia.routingTable.AddContact(testContact)
+	//fmt.Println("routingTable.buckets[0]: ", *kademlia.routingTable.buckets)
 	//routingTable := RoutingTable()
 	//k := kademlia(){}
 }
